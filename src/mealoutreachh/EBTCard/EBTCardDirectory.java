19d8fecf9e61c73b13c6f558a90df1dc672489cb
mundargishruti <mundargi.s@northeastern.edu>
2022-12-06 17:08:58 -0500

    Ebt class and directory created

---
+++ src/mealoutreachh/EBTCard/EBTCardDirectory.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mealoutreachh.EBTCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author shruti
+ */
+public class EBTCardDirectory {
+    private List<EBTCard> EBTCards = new ArrayList<>();
+
+    public List<EBTCard> getEBTCards() {
+        return EBTCards;
+    }
+
+    public void setEBTCards(List<EBTCard> EBTCards) {
+        this.EBTCards = EBTCards;
+    }
+}
