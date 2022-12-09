88226ae20173c0bea19a4f8a8f1d76b4557dad2b
Rajvi-C <chokshi.ra@northeastern.edu>
2022-12-08 23:03:02 -0500

    Enterprise + directory Added

---
+++ src/code/Enterprise/EnterpriseDirectory.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package code.Enterprise;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rajvi
+ */
+public class EnterpriseDirectory {
+    private ArrayList<Enterprise> enterpriseList;
+   
+
+    public ArrayList<Enterprise> getEnterpriseList() {
+        return enterpriseList;
+    }
+
+    public void setEnterpriseList(ArrayList<Enterprise> entList) {
+        this.enterpriseList = entList;
+    }
+    
+    public EntDirectory(){
+        enterpriseList=new ArrayList<Enterprise>();
+    }
+    
+    //Create enterprise
+    public Enterprise createAndAddEnterprise(String name,Enterprise.EnterpriseType entType){
+        
+        Enterprise ent=null;
+        
+        if(entType==Enterprise.EnterpriseType.FoodBankEnterprise){
+            ent=new FoodBankEnterprise(name);
+            enterpriseList.add(ent);
+        }
+        
+        else if(entType == Enterprise.EnterpriseType.FDAEnterprise)
+        {
+            ent =  new FDAEnterprise(name);
+            enterpriseList.add(ent);
+            
+        }
+        
+        else if(entType == Enterprise.EnterpriseType.EBTEnterprise)
+        {
+            ent = new EBTEnterprise(name);
+            enterpriseList.add(ent);
+            
+            
+        }
+        else if(entType == Enterprise.EnterpriseType.NGOEnterprise)
+        {
+            ent = new NGOEnterprise(name);
+            enterpriseList.add(ent);
+            
+        }
+        return ent;
+    }
+}
