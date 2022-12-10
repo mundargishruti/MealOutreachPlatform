eca62f5dfe01a18b527ae49fe4d4f5bc6e3e3e72
mundargishruti <mundargi.s@northeastern.edu>
2022-12-09 20:05:30 -0500

    added NGO enterprise

---
+++ src/code/Enterprise/NGOEnterprise.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package code.Enterprise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author shruti
+ */
+public class NGOEnterprise extends Enterprise{
+    public NGOEnterprise(String name) {
+        super(name, Enterprise.EnterpriseType.NGOEnterprise);
+    }
+    
+     @Override
+    public ArrayList<Role> getSupportRole() {
+        return null;
+    }
+
+    @Override
+    public List<Organization.Type> getAllOrgTypes() {
+        List<Organization.Type> orgnanizationTypes = new ArrayList<>();
+        orgnanizationTypes.add(Organization.Type.NGOAdministrator);
+        orgnanizationTypes.add(Organization.Type.Director);
+
+        return orgnanizationTypes;
+    }
+    
+}
