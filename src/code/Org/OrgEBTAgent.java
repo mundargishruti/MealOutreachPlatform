39272a744ff8f8e89fb19cbb7559bb467531835e
mundargishruti <mundargi.s@northeastern.edu>
2022-12-09 21:38:52 -0500

    EBT organizations

---
+++ src/code/Org/OrgEBTAgent.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package code.Org;
+
+import code.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author shruti
+ */
+public class OrgEBTAgent extends Organization {
+
+    public OrgEBTAgent() {
+        super(Organization.Type.EBTAgent.getValue());
+    }
+
+    @Override
+    public ArrayList<Role> getSupportRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new RoleEbtAgent());
+        return roles;
+    }
+}
+    
+
