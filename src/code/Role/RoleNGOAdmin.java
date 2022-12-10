ef6d663a02880def171eabbce51b1d5774a08dd5
HemantJomraj <jomraj.h@northeastern.edu>
2022-12-09 23:10:20 -0500

    Role NGO Admin

---
+++ src/code/Role/RoleNGOAdmin.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package code.Role;
+import code.EcoSystem;
+import code.Enterprise.Enterprise;
+import code.Org.OrgNGOAdmin;
+import code.Org.Organization;
+import code.SystemUserAccount.SystemUsers;
+import javax.swing.JPanel;
+import code.NGOAdministratorRole.NGOAdministratorWorkAreaPanel;
+import UI.LabAssistantRole.LabAssistantWorkAreaJPanel;
+
+/**
+ *
+ * @author jomraj
+ */
+public class RoleNGOAdmin extends Role {
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, SystemUsers account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new NGOAdministratorWorkAreaPanel(userProcessContainer, account, (OrgNGOAdmin)organization, enterprise);
+    }
+}
