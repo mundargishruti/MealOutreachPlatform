77b7c38ab4b0977f18ce1dd406e08c8f21fe1bb5
Rajvi-C <chokshi.ra@northeastern.edu>
2022-12-09 21:54:48 -0500

    Organisation Accountant modified

--- src/code/Org/OrgAccountant.java
+++ src/code/Org/OrgAccountant.java
@@ -4,10 +4,26 @@
  */
 package code.Org;
 
+import code.Role.RoleAccountant;
+import code.Role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author Rajvi
  */
-public class OrgAccountant {
-    
+public class OrgAccountant extends Organization {
+
+    public OrgAccountant() {
+        super(Organization.Type.Accountant.getValue());
+
+    }
+
+    @Override
+    public ArrayList<Role> getSupportRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new RoleAccountant());
+        return roles;
+    }
+
 }
