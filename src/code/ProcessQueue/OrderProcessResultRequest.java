3a7563bcbc88439053bf04763a44954059889573
HemantJomraj <jomraj.h@northeastern.edu>
2022-12-11 00:59:45 -0500

    OrderProcessResultRequest

---
+++ src/code/ProcessQueue/OrderProcessResultRequest.java
@@ -0,0 +1,22 @@
+
+package code.ProcessQueue;
+
+/**
+ *
+ * @author jomraj
+ */
+public class OrderProcessResultRequest extends ProcessRequest{
+    
+    private String tstRslt; //Test Results
+
+    //Getters and Setters
+    public String getTstRslt() {
+        return tstRslt;
+    }
+
+    public void setTstRslt(String tstRslt) {
+        this.tstRslt = tstRslt;
+    }
+    
+    
+}
